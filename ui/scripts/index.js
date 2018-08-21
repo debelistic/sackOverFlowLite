50adb9a213c2fd0d7cf9571f66436bff073651f8
Victor Awotidebe <victorawotidebe@gmail.com>
2018-08-21 07:26:16 +0100

    [Chore #]

---
+++ ui/scripts/index.js
@@ -0,0 +1,20 @@
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function disableSubbmit() {
+    document.getElementsByClassName("formgroup").disabled = true;
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    let dropdowns = document.getElementsByClassName("dropdown-content");    
+    for (let i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
