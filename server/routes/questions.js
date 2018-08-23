36e84a940a3dc336b8c93017d12d054f54e2573b
Victor Awotidebe <victorawotidebe@gmail.com>
2018-08-23 13:39:11 +0100

    [Feature #159984635] Added get all questions route

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -3,7 +3,7 @@ import questions from '../data/data';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/api/', (req, res) => {
 	// res.status(200).send({
 	// 	success: "true",
 	// 	message: "questions successful retrived",
@@ -13,7 +13,7 @@ router.get('/', (req, res) => {
 	res.send(questions);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/api/questions/:id', (req, res) => {
 	const question = questions.find(q => q.id === parseInt(req.params.id));
 	if(!question) res.status(404).send('question not fond');
 	res.send(question);
